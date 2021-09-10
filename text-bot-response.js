63ebff7f4ffad2d840cf3c83a019061db683ff61
Tuan Dung HOANG <tuandung.hoang0228@gmail.com>
2021-09-10 21:47:51 +0200

    Update text-bot-response.js

--- text-bot-response.js
+++ text-bot-response.js
@@ -1,5 +1,5 @@
 output.answer = 'Merci de saisir votre adresse ?'; // once per Nodejs
 
-messenger_answer.push({ text: 'Merci de saisir votre adresse ?'});
+// messenger_answer.push({ text: 'Merci de saisir votre adresse ?'});
 
 callback(output);
